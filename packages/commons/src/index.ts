3f1225b2279fd4ad3902fea842aa9a8bf6673dca
Alexander Bolanos <alexander.bl85@gmail.com>
2026-05-25 14:03:51 -0600

    Agregada configuración básica del paquete commons

---
+++ packages/commons/src/index.ts
@@ -0,0 +1,10 @@
+// DTOs
+export * from "./dtos/create-rsvp.dto";
+export * from "./dtos/catalog-item.dto";
+
+// Types
+export * from "./types/discount.types";
+export * from "./types/event.types";
+
+// Constants
+export * from "./constants/discount.constants";
